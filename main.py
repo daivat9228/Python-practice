1f8bf1b9e381ca5fd1ff2f2560f9ca37cc5477c8
Daivat Dhimmar <daivat796@gmail.com>
2026-05-29 22:59:06 +0530

    feat: add practice script demonstrating Python variables, data types, and type conversion

---
+++ main.py
@@ -0,0 +1,118 @@
+# comment and variables 
+    # this line below used for printing 
+print("hello dev")
+""" 
+this 
+is 
+multiline
+comment"""
+
+# variables 
+# camelCase  →
+myVariableName = "d"
+# PascalCase →
+MyVariableName = "de"
+# snake_case →
+my_variable_name = "dev"
+
+
+
+# Data types
+# a = -23
+# b = 45
+# c = 56
+
+#int - 0 to infinity and -1 to negative infinity
+# print(type(c))
+
+# sher = 12/3
+
+# print(type(sher))
+
+#float are the values which include decimal and fraction 
+
+# a = 12 + 3j
+
+# print(type(a))
+
+#string you can save anything inside quotes
+# name = 'yo bro he said "you are a bad person"'
+
+# print(name)
+
+
+#boolean
+
+# a = "True" 
+# b = False  
+
+# print(type(a))
+
+# Strings & Type Conversion
+a = "Hello how are you"
+
+#how 
+
+#you
+
+#Hello
+
+#type conversion 
+
+# a = " "
+
+# print(ord(a))
+
+a = "Hello how are you"
+
+#how 
+
+#you
+
+#Hello
+
+
+
+
+#type conversion 
+
+# a = "12.5"
+# a = int(a)
+
+# print(a)
+
+#you can convert string if it holds valid integers
+#you can convert float values to int
+
+# a = 143
+
+# a = float(a)
+# print(a)
+
+# a = 123
+# b = 34.5
+# c = 12 + 34j
+# d = True
+
+# a = str(a)
+# b = str(b)
+# c = str(c)
+# d = str(d)
+
+
+# a = 12 
+# b = 0 
+# c = 12.4
+# d = 0.0 
+# e = ""
+# f = "hello"
+# print(bool(a))
+# print(bool(b))
+# print(bool(c))
+# print(bool(d))
+# print(bool(e))
+# print(bool(f))
+
+# a = 12 
+
+# print(a/2)
